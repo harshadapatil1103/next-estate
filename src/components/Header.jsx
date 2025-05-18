3e73a2e3b9603672806e6b6c7d6d8c602ec7df00
harshada <11harshadapatil@gmail.com>
2025-05-18 17:34:26 +0530

    creating pull request

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,8 +32,9 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link href='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
+            <span className='text-slate-500'></span>
             <span className='text-slate-700'>Estate</span>
+            <span>Git changes</span>
           </h1>
         </Link>
         <form
